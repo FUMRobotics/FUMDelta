0322be7e41acf244e211a59e4d551fbe3998896f
taraneh qandi <taranehqandi@gmail.com>
2019-07-29 16:05:39 +0430

    added message handler to write all logs to a file (called: "debug-log")

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include "receiver.h"
+#include <QtGlobal>
+#include <stdio.h>
+#include <stdlib.h>
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    QString txt;
+    switch (type) {
+    case QtDebugMsg:
+        txt= QString("Debug: %1 (%2:%3, %4)\n").arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
+
+        break;
+    case QtInfoMsg:
+        txt= QString("Info: %1 (%2:%3, %4)\n").arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
+        break;
+    case QtWarningMsg:
+        txt= QString("Warning: %1 (%2:%3, %4)\n").arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
+        break;
+    case QtCriticalMsg:
+        txt= QString("Critical: %1 (%2:%3, %4)\n").arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
+        break;
+    case QtFatalMsg:
+        txt= QString("Fatal: %1 (%2:%3, %4)\n").arg(localMsg.constData()).arg(context.file).arg(context.line).arg(context.function);
+        abort();
+    }
+    QFile outFile("debug-log");
+    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream ts(&outFile);
+    ts << txt << endl;
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(myMessageOutput); // Install the handler
     QApplication a(argc, argv);
     Receiver receiver;
     MainWindow w(&receiver);
