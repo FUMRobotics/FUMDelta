53f5fc0af6f582914af8d2a539a9a08baba814fd
Taraneh <Qandi>
2019-09-16 10:29:13 +0430

    testing circ and linear in main

--- main.cpp
+++ main.cpp
@@ -100,22 +100,6 @@ int main(int argc, char *argv[])
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     //start ethercat
     QProcess *ethercatProcess=new QProcess() ;
     QProcess process1;
@@ -133,6 +117,7 @@ int main(int argc, char *argv[])
     //start drives
     //start ethercat and drives
     Receiver receiver;
+
     MainWindow w(&receiver);
 
     receiver.start();
@@ -150,11 +135,43 @@ int main(int argc, char *argv[])
     qDebug()<< output2  << stderr;
 
 
-    InterpreterCore* core=new InterpreterCore();
-    core->base();
+//    InterpreterCore* core=new InterpreterCore();
+//    core->base();
     qDebug("is running...");
 
 
+
+
+
+
+    /*------------------------------------TESTING CIRC & LINEAR-------------------------------------*/
+//    InverseKinematicsCore core;
+////    core.circ(-200,100,-700, 0, 0, 0, 0, 200,0,-800, 0, 0, 3.141592653589793, 0, 25, 10, 100, 50,100,-820,0);
+
+//    core.linear(0, 0, -755,0,0, 3.141592653589793,0, 200,200 , -800, 0,0, 3.141592653589793 / 2,0,25,10,100);
+//    QVector<double> m1;
+//    QVector<double> m2;
+//    QVector<double> m3;
+
+//    QVector<QVector<double>>points;
+//    points.push_back(m1);
+//    points.push_back(m2);
+//    points.push_back(m3);
+
+//    for (int var = 0; var < core.size_of_trajectory_points; var++) {
+//        qDebug("M1[%d]=%lf",var,core.M1[var]);
+//        qDebug("M2[%d]=%lf",var,core.M2[var]);
+//        qDebug("M3[%d]=%lf\n",var,core.M3[var]);
+
+//        m1.push_back(core.M1[var]);
+//        m2.push_back(core.M2[var]);
+//        m3.push_back(core.M3[var]);
+//    }
+
+//    for (int var = 0; var < core.size_of_trajectory_points; var++) {
+//        SendCommand::getInstance()->SendPointTo3Drives(core.M1[var],core.M2[var],core.M3[var]);
+//    }
+/*------------------------------------TESTING CIRC & LINEAR-------------------------------------*/
 
     //-----------------------------------------------------------------------------------------------------------
     /*---------------------------TESTING INVERSE--------------------------*/
